921391e94bcc81369c14669126610ee2093d7eeb
antoine drabble <antoine.drabble@gmail.com>
2017-01-10 13:41:23 +0100

    Update

--- DiffusionServeur/src/DiffusionServeur.java
+++ DiffusionServeur/src/DiffusionServeur.java
@@ -3,12 +3,13 @@
  * Authors: Antoine Drabble & Simon Baehler
  * Date: 20.12.2016
  */
+import javafx.util.Pair;
+
 import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.SocketException;
-import java.util.Arrays;
+import java.net.*;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  *
@@ -17,13 +18,17 @@ public class DiffusionServeur {
     // Création du socket point à point pour l'envoi de packet UDP
     int port;
     DatagramSocket pointAPointSocket;
+    ArrayList<Site> voisins = new ArrayList<>();
+    int numberoSite = 1;
+    short idCpt = 0;
 
     public DiffusionServeur(int port) throws SocketException {
         port = port;
         pointAPointSocket = new DatagramSocket(port);
     }
 
-    public void demarrer(){
+    public void demarrer() throws IOException {
+        HashMap<Integer,Integer> l = new HashMap<>();
         while(true){
             byte[] buffer = new byte[250];
             DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
@@ -36,30 +41,39 @@ public class DiffusionServeur {
                 case 0:
                     System.out.println("Received message from client");
                     System.out.println(new String(packet.getData()));
+                    // TODO : Transformer l'id en tableau de byte de longueur 8 bytes : ip - port - cpt
+                    int id = idCpt++ * 10 + numberoSite;
+                    l.put(id, voisins.size());
+                    for(Site voisin : voisins){
+                        // TODO cast id en byte array
+                        // TODO ajouter le byte array packet.getData() enlever le premier byte
+                        buffer = new byte[]{1, (byte)numberoSite, id, packet.getData()};
+                        packet = new DatagramPacket(buffer, buffer.length, InetAddress.getByName(voisin.getIp()), voisin.getPort());
+                        pointAPointSocket.send(packet);
+                    }
                     break;
                 case 1:
+                    (ID,msg') ¬ msg -- Décomposition du contenu de msg
+                    if ID Î L then
+                        Retirer (ID,a) de L
+                        if a > 1 then
+                            L ¬ L È (ID,a-1)
+                        end if
+                    else
+                        "j Î Voisini - {émetteur}, envoyer(sonde,i,msg) à j
+                        envoyer(écho,i,ID) à émetteur
+                        envoyer(msg') à l'application locale
+                        L ¬ L È (ID,| Voisini | - 1)
+                    end if
                     break;
                 case 2:
+                    byte[] idBuffer = {packet.getData()[1], packet.getData()[2], packet.getData()[3], packet.getData()[4], packet.getData()[5], packet.getData()[6]};
+                    Retirer (msg,a) de L
+                    if a > 1 then
+                    L ¬ L È (msg,a-1)
+                    end if
                     break;
             }
-/*
-
-            // Envoi du paquet de souscription
-            DatagramPacket linkerSubscribePacket = new DatagramPacket(souscriptionBuffer, souscriptionBuffer.length, InetAddress.getByName(lieurs[linkerNumber].getIp()), lieurs[linkerNumber].getPort());
-            pointAPointSocket.send(linkerSubscribePacket);
-
-            // Attente de la confirmation du lieur
-            byte[] buffer = new byte[1];
-            DatagramPacket linkerConfirmationPacket = new DatagramPacket(buffer, buffer.length);
-            try {
-                pointAPointSocket.setSoTimeout(tempsMaxAttenteReponse);
-                pointAPointSocket.receive(linkerConfirmationPacket);
-            } catch (SocketException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-*/
         }
     }
 
---
+++ DiffusionServeur/src/Protocole.java
@@ -0,0 +1,8 @@
+/**
+ * Created by antoi on 10.01.2017.
+ */
+public enum Protocole {
+    LOCAL,
+    SONDE,
+    ECHO
+}
